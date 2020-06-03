111e92c3ac04c2b0f73adffd326c278316edf3db
Eric Mauldin <emauldin84@gmail.com>
2020-06-03 16:28:23 -0400

    inital commit with working markdown preview

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
+import marked from 'marked'
+
 function App() {
+  const [preview, setPreview] = useState('')
+  const [text, setText] = useState(defaultText)
+
+  useEffect(() => {
+    handleSetPreview(text)
+  }, [])
+
+  const handleSetPreview = (text) => {
+    let mark = marked(text)
+    console.log(mark)
+    setPreview(mark)
+  }
+
+  const handleSetText = (e) => {
+    setText(e.target.value)
+    handleSetPreview(e.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea onChange={handleSetText} value={text} rows="20" cols="80"/>
+      <div dangerouslySetInnerHTML={{__html: preview}}/>
     </div>
   );
 }
 
 export default App;
+
+
+const defaultText = "# Header \n\
+## Sub Header \n\
+[I'm an inline-style link](https://www.google.com) \n\
+\n\
+`inline code` \n\
+\n\
+```block code``` \n\
+1. list item 1 \n\
+2. list item 2 \n\
+\n\
+> block quote\n\
+\n\
+inline image\n\
+![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png \"Logo Title Text 1\")\n\
+\n\
+**bold text**\n\
+"
